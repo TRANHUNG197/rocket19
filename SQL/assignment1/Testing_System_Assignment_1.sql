9c3a21baa952422c9e324a26135b756ddd22b43a
TRANHUNG197 <haohoacp@gmail.com>
2021-09-15 18:38:28 +0900

    submit assignment1

---
+++ SQL/assignment1/Testing_System_Assignment_1.sql
@@ -0,0 +1,89 @@
+DROP database if exists testing_system_1;
+create database if not exists testing_system_1;
+use testing_system_1;
+
+CREATE TABLE Department 
+(
+    DepartmentID 	TINYINT AUTO_INCREMENT PRIMARY KEY,
+    DepartmentName 	VARCHAR(50)
+);
+
+CREATE TABLE Position
+(
+    PositionID 		TINYINT AUTO_INCREMENT PRIMARY KEY,
+    PositionName 	ENUM('Dev1', 'Dev2', 'Test', 'Leader', 'Scrum Master', 'PM')
+);
+
+CREATE TABLE `Account`
+(
+    AccountID 		INT AUTO_INCREMENT PRIMARY KEY,
+    Email 			VARCHAR(100),
+    Username		VARCHAR(50),
+    FullName		VARCHAR(50),
+    DepartmentID	TINYINT,
+    PositionID	 	TINYINT,
+    CreateDate		datetime
+    
+);
+
+CREATE TABLE `Group`
+(
+    GroupID		INT AUTO_INCREMENT PRIMARY KEY,
+	GroupName	VARCHAR(50),
+    CreatorID	INT,
+    CreateDate	datetime
+);
+
+CREATE TABLE GroupAccount
+(
+    GroupID		INT,
+	AccountID	INT,
+    JoinDate	datetime
+);
+
+CREATE TABLE TypeQuestion
+(
+    TypeID		TINYINT AUTO_INCREMENT PRIMARY KEY,
+	TypeName	VARCHAR(50)
+);
+
+CREATE TABLE CategoryQuestion
+(
+    CategoryID			TINYINT AUTO_INCREMENT PRIMARY KEY,
+	CategoryName		ENUM ('Java', 'SQL', '.NET', 'Ruby', 'Python', 'NodeJS' , 'HTML', 'CSS', 'JavaScript')
+);
+
+CREATE TABLE Question
+(
+    QuestionID			TINYINT AUTO_INCREMENT PRIMARY KEY,
+	Content				VARCHAR(200),
+    CategoryID			INT,
+    TypeID				INT,
+    CreatorID			INT,
+    CreateDate			datetime
+);
+
+CREATE TABLE Answer
+(
+    AnswerID			TINYINT AUTO_INCREMENT PRIMARY KEY,
+	Content				VARCHAR(200),
+    QuestionID			INT,
+    isCorrect			BIT
+);
+
+CREATE TABLE Exam
+(
+    ExamID				INT AUTO_INCREMENT PRIMARY KEY,
+	`Code`				VARCHAR(30),
+    Title				VARCHAR(50),
+    CategoryID			INT,
+    Duration			INT,
+    CreatorID			INT,
+    CreateDate			datetime
+);
+
+CREATE TABLE ExamQuestion
+(
+    ExamID				INT PRIMARY KEY,
+	QuestionID			INT
+);
---
+++ SQL/assignment2/Testing_System_Assignment_2.sql
@@ -0,0 +1,136 @@
+use Testing_System_1;
+
+/* INSERT DATA bang Department */
+INSERT INTO Department (DepartmentName)
+VALUES ('Phong Ky Thuat 1'),
+       ('Phong Ky Thuat 2'),
+       ('Phong Dev 1'),
+       ('Phong Dev 2'),
+       ('Phong Sale'),
+       ('Phong Marketing'),
+       ('Phong Giam Doc'),
+       ('Phong Tai Chinh');
+       
+/* INSERT DATA bang Position */
+INSERT INTO Position (PositionName)
+VALUES ('Dev1'),
+       ('Dev2'),
+       ('Test'),
+       ('PM'),
+       ('Leader'),
+       ('Scrum Master');
+       
+/* INSERT DATA bang Account */
+INSERT INTO Account (Email, Username, Fullname, DepartmentID, PositionID, CreateDate)
+VALUES ('vti_account1@vtiacademy.com', 'vti1', 'Nguyen Van Tinh', 1, 1, '2019-12-01'),
+       ('vti_account2@vtiacademy.com', 'vti2', 'Trinh Hoai Linh', 1, 2, '2020-12-01'),
+       ('vti_account3@vtiacademy.com', 'vti3', 'Nguyen Van Test', 1, 1, '2020-07-01'),
+       ('vti_account4@vtiacademy.com', 'vti4', 'Tran Van Tinh', 1, 2, '2019-09-01'),
+       ('vti_account5@vtiacademy.com', 'account5', 'Ho Van Tinh', 3, 2, '2021-07-01'),
+       ('vti_account6@vtiacademy.com', 'account_vti6', 'Cao Thai Son', 3, 1, NOW()),
+       ('vti_7@vtiacademy.com', 'account_vti7', 'Tam Thất Tùng', 3, 3, '2020-10-01'),
+       ('vti_8@vtiacademy.com', 'account_vti8', 'Nguyen Quynh Thu', 3, 4, '2019-04-01'),
+       ('vti_9@vtiacademy.com', 'account_vti9', 'Tran Kim Tuyen', 2, 1, NOW()),
+       ('vti_account10@vtiacademy.com', 'account_vti10', 'Nguyen Ba Dao', 1, 5, '2019-10-01'),
+       ('vti_account11@vtiacademy.com', 'account_vti11', 'Nguyen Van Binh', 1, 3, '2020-12-01');
+       
+/* INSERT DATA bang Group */
+INSERT INTO `Group` (GroupName, CreatorID, CreateDate)
+VALUES ('Nhom 1', '3', '2021-04-03'),
+       ('Nhom 2', '3', '2019-01-03'),
+       ('Nhom 3', '2', '2020-04-03'),
+       ('Nhom 4', '1', NOW()),
+       ('Nhom 5', '3', '2021-06-03'),
+       ('Nhom 6', '1', '2020-04-03'),
+       ('Nhom 7', '5', '2021-04-03'),
+       ('Nhom 8', '5', '2019-05-03'),
+       ('Nhom 9', '3', '2019-01-03'),
+       ('Nhom 10', '1', NOW());
+       
+/* INSERT DATA bang GroupAccount */
+INSERT INTO GroupAccount (GroupID, AccountID, JoinDate)
+VALUES ('1', '1', '2021-06-01'),
+       ('1', '3', '2020-01-01'),
+       ('1', '2', NOW()),
+       ('1', '4', '2021-06-01'),
+       ('2', '1', '2021-06-01'),
+       ('2', '10', '2019-05-01'),
+       ('5', '1', '2021-06-01'),
+       ('5', '3', '2020-01-01'),
+       ('5', '4', '2021-07-01'),
+       ('3', '1', '2021-06-01'),
+       ('9', '2', '2021-06-01'),
+       ('10', '1', NOW());
+       
+/* INSERT DATA bang TypeQuestion */
+INSERT INTO TypeQuestion (TypeName)
+VALUES ('Trac nghiem'),
+       ('Tu Luan');
+       
+/* INSERT DATA bang CategoryQuestion */
+INSERT INTO CategoryQuestion (CategoryName)
+VALUES ('Java'),
+       ('SQL'),
+       ('HTML'),
+       ('CSS '),
+       ('.NET'),
+       ('Python'),
+       ('Ruby'),
+       ('JavaScript');
+	
+/* INSERT DATA bang Question */
+INSERT INTO Question (Content, CategoryID, TypeID, CreatorID, CreateDate)
+VALUES ('Câu hỏi SQL 1', 2, 2, 1, '2021-04-01'),
+       ('Câu hỏi SQL 2', 2, 2, 2, '2020-01-01'),
+       ('Câu hỏi JAVA 1', 1, 1, 10, '2021-07-01'),
+       ('Câu hỏi JAVA 2', 1, 2, 5, '2021-06-01'),
+       ('Câu hỏi HTML 1', 3, 1, 2, NOW()),
+       ('Câu hỏi HTML 2', 3, 2, 2, NOW());
+       
+/* INSERT DATA bang Answer */
+INSERT INTO Answer (Content, QuestionID, isCorrect)
+VALUES ('Câu trả lời 1 - question SQL 1', 1, 1),
+       ('Câu trả lời 2 - question SQL 1', 1, 0),
+       ('Câu trả lời 3 - question SQL 1', 1, 0),
+       ('Câu trả lời 4 - question SQL 1', 1, 1),
+       ('Câu trả lời 1 - question SQL 2', 2, 0),
+       ('Câu trả lời 2 - question SQL 2', 2, 0),
+       ('Câu trả lời 3 - question SQL 2', 2, 0),
+       ('Câu trả lời 4 - question SQL 2', 2, 1),
+       ('Câu trả lời 1 - question JAVA 1', 3, 0),
+       ('Câu trả lời 2 - question JAVA 1', 3, 1),
+       ('Câu trả lời 1 - question JAVA 2', 4, 0),
+       ('Câu trả lời 2 - question JAVA 2', 4, 0),
+       ('Câu trả lời 3 - question JAVA 2', 4, 0),
+       ('Câu trả lời 4 - question JAVA 2', 4, 1),
+       ('Câu trả lời 1 - question HTML 1', 5, 1),
+       ('Câu trả lời 2 - question HTML 2', 5, 0);
+       
+/* INSERT DATA bang Exam */
+INSERT INTO Exam (`Code`, Title, CategoryID, Duration, CreatorID, CreateDate)
+VALUES ('MS_01', 'De thi 01', 1, 90, 1, NOW()),
+       ('MS_02', 'De thi 02', 1, 60, 5, NOW()),
+       ('MS_03', 'De thi 03', 2, 60, 9, NOW()),
+       ('MS_04', 'De thi 04', 2, 90, 1, NOW()),
+       ('MS_05', 'De thi 05', 1, 60, 2, NOW()),
+       ('MS_06', 'De thi 06', 2, 90, 2, NOW()),
+       ('MS_07', 'De thi 07', 1, 60, 1, NOW());
+       
+/* INSERT DATA bang Examquestion */
+INSERT INTO ExamQuestion
+VALUES (1, 1),
+       (2, 1),
+       (3, 1),
+       (4, 1),
+       (5, 2),
+       (6, 2),
+       (7, 2),
+       (8, 2),
+       (9, 3),
+       (10, 3),
+       (11, 4),
+       (12, 4),
+       (13, 4),
+       (14, 4),
+       (15, 5),
+       (16, 5);
