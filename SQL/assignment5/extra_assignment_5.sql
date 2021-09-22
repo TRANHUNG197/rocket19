6ecf33427f38e4ab53e82a1466887a977ad31339
TRANHUNG197 <haohoacp@gmail.com>
2021-09-23 00:56:53 +0900

    assignment 5 and extra-assignment 5

--- SQL/assignment5/extra_assignment_5.sql
+++ SQL/assignment5/extra_assignment_5.sql
@@ -14,10 +14,7 @@ select `name` from product where ProductSubcategoryID = (select ProductSubcatego
 
 /*Question 2: Thay đổi câu Query 1 để lấy được kết quả sau.*/
 
-
-select `name` from product where (substring_index(`name`, ' ', 1)) like'bo%';
-
-select `name` from product where ProductSubcategoryID = (select ProductSubcategoryID from productsubcategory where 'bo%');
+select `name` from product where ProductSubcategoryID in (select ProductSubcategoryID from productsubcategory where `name` like 'bo%');
 
 
 /*Question 3:
@@ -26,7 +23,7 @@ Bike (nghĩa là ProductSubcategoryID = 3)
 Hướng dẫn: sử dụng hàm MIN() trong subquery để trả về ListPrice thấp nhất*/
 
 
-select `name` from product where ProductSubcategoryID =  3  having (select min(ListPrice) from product);
+select `name` from product where ProductSubcategoryID =  3  and listprice = (select min(ListPrice) from product where ProductSubcategoryID =  3);
 
 select min(ListPrice) from product;
 
@@ -37,5 +34,38 @@ select ProductID from product where min(ListPrice) group by listprice;
 /*Question 1: Viết query lấy danh sách tên country và province được lưu trong
 AdventureWorks2008sample database.*/
 
+select * from stateprovince;
 
+select ct.name, st.name
+from countryregion ct 
+join stateprovince st on ct.CountryRegionCode = st.CountryRegionCode;
+
+
+/*Question 2: Tiếp tục với câu query trước và thêm điều kiện là chỉ lấy country
+Germany và Canada
+Chú ý: sủ dụng sort order và column headings*/
+
+select ct.name, st.name
+from countryregion ct 
+join stateprovince st on ct.CountryRegionCode = st.CountryRegionCode
+where ct.name = 'Germany' or ct.name = 'Canada';
+
+/*Question 3:
+SalesOrderID, OrderDate and SalesPersonID. Từ bảng SalesPerson, chúng ta lấy cột
+BusinessEntityID (là định danh của người sales), Bonus and the SalesYTD (là đã sale
+được bao nhiêu người trong năm nay)*/
+
+select SalesOrderID, OrderDate, soh.SalesPersonID, sp.SalesPersonID as BusinessEntityID, sp.Bonus, sp.SalesYTD
+from salesperson sp
+join salesorderheader soh on sp.SalesPersonID = soh.SalesPersonID;
+
+
+/*Question 4:
+Sử dụng câu query ở question 3, thêm cột JobTitle and xóa cột SalesPersonID và
+BusinessEntityID.*/
+
+select SalesOrderID, OrderDate, e.Title as JobTitle, sp.Bonus, sp.SalesYTD
+from salesperson sp 
+join salesorderheader soh on sp.SalesPersonID = soh.SalesPersonID
+join employee e using(ContactID); 
 
